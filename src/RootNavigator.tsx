d93453fc6fc3d3b16c757e50eaab6a21dde71ef4
xesk84 <fj.aparisi@gmail.com>
2020-03-01 16:11:06 +0100

    cleaning

--- src/RootNavigator.tsx
+++ src/RootNavigator.tsx
@@ -1,36 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {DeliveriesList, DeliveryDetail, DeliveryDetailScreen} from './screens';
+import {DeliveriesList, DeliveryDetailScreen} from './screens';
 import {createStackNavigator} from '@react-navigation/stack';
-// import { useUserContext } from './providers/UserProvider';
-// import { User } from './entities/User';
-// import { GetGlobalUser } from './services/AsyncStorageService';
 
 export function RootNavigation () {
     const Stack = createStackNavigator();
-    
-    
-    // const [loading, setLoading] = useState(true);
-    // const [logged, setLogged] = useState(false);
-    // const [user, setUser] = useUserContext();
-    
-    // useEffect(() => {
-    //     LoadGlobalData();
-    // });
-    
-    // const LoadGlobalData = async () => {
-    //     setLoading(true);
-    //     if(!user || user.id === 0){
-    //         let asyncStorageUser: User = await GetGlobalUser();
-    //         if(!!asyncStorageUser && asyncStorageUser.id > 0){
-    //             setUser(asyncStorageUser);
-    //             setLogged(true);
-    //         }
-    //     }
-    //     setLoading(false);
-    //   };
-
-
     return (
         <NavigationContainer>
         <Stack.Navigator initialRouteName="List">
