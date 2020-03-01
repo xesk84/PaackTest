75805790062c538193d49375934b9e49c2d9e8e8
xesk84 <fj.aparisi@gmail.com>
2020-03-01 16:03:20 +0100

    background process working without context

--- index.js
+++ index.js
@@ -5,67 +5,33 @@ import BackgroundJob from 'react-native-background-job';
 
 AppRegistry.registerComponent(appName, () => App);
 
-
-/*
-const backgroundJob = {
- jobKey: "myJob",
- allowWhileIdle: true,
- allowExecutionInForeground: true, 
- job: () => console.log("Running in background")
-};
-
-BackgroundJob.register(backgroundJob);
-
-var backgroundSchedule = {
-    jobKey: "myJob",
-    allowWhileIdle: true,
-    allowExecutionInForeground: true
-   }
-   
-   BackgroundJob.schedule(backgroundSchedule)
-     .then(myTest)
-     .catch(err => console.err(err));
-
-function myTest() {
-    setInterval(function(){
-        console.log('Holis')
-    }, 1000);
-    console.log('tu puta madre');
-}
-*/
-
-
-
-const backgroundJob = {
-    jobKey: "myJob",
-    job: () => { 
-      locat(); 
-     }
-   };
+// const backgroundJob = {
+//     jobKey: "myJob",
+//     job: () => { 
+//       saveGPSData(); 
+//      }
+//    };
   
-  BackgroundJob.register(backgroundJob);
+//   BackgroundJob.register(backgroundJob);
   
-  var backgroundSchedule = {
-   jobKey: "myJob",
-   period: 2000,
-   allowWhileIdle: true,
-   exact: true,
-   //requiresCharging: true,
-   requiresDeviceIdle: true,
-   override: false,
-   timeout: 3000,
-   allowExecutionInForeground: true,
-  }
+//   var backgroundSchedule = {
+//    jobKey: "myJob",
+//    period: 2000,
+//    allowWhileIdle: true,
+//    exact: true,
+//    //requiresCharging: true,
+//    //requiresDeviceIdle: true,
+//    //override: false,
+//    //timeout: 3000,
+//    allowExecutionInForeground: true,
+//   }
   
-  BackgroundJob.schedule(backgroundSchedule);
+//   BackgroundJob.schedule(backgroundSchedule);
  
-  BackgroundJob.isAppIgnoringBatteryOptimization((error,ignoringOptimization) => { 
-    console.log(error+' <<Android>>ignore>> '+ignoringOptimization) 
-   });
+//   BackgroundJob.isAppIgnoringBatteryOptimization((error,ignoringOptimization) => { 
+//     console.log(error+' <<Android>>ignore>> '+ignoringOptimization) 
+//    });
  
- const locat = (data) => {
-//    navigator.geolocation.getCurrentPosition((position) => {
-//      console.log('Android>>: Location>>222--->>: '+JSON.stringify(position.coords));
-//     });
-console.log('holis');
- }
+//  const saveGPSData = (data) => {
+//   console.log('holis');
+//  }
--- src/RootNavigator.tsx
+++ src/RootNavigator.tsx
@@ -1,41 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {DeliveriesList, DeliveryDetail, DeliveryDetailScreen} from './screens';
+import {DeliveriesList, DeliveryDetailScreen} from './screens';
 import {createStackNavigator} from '@react-navigation/stack';
-// import { useUserContext } from './providers/UserProvider';
-// import { User } from './entities/User';
-// import { GetGlobalUser } from './services/AsyncStorageService';
+import BackgroundJob from 'react-native-background-job';
+import { useSelectedDeliveryContext } from './providers/SelectedDelivery';
+
 
 export function RootNavigation () {
+    const [selectedDelivery, setSelectedDelivery] = useSelectedDeliveryContext();
+
+    const backgroundJob = {
+        jobKey: 'myJob',
+        job: (selectedDelivery) => { 
+          saveGPSData(selectedDelivery); 
+         }
+       };
+      
+      BackgroundJob.register(backgroundJob);
+      
+      var backgroundSchedule = {
+       jobKey: 'myJob',
+       period: 1000,
+       allowWhileIdle: true,
+       exact: true,
+       allowExecutionInForeground: true,
+      }
+      
+      BackgroundJob.schedule(backgroundSchedule);
+     
+      BackgroundJob.isAppIgnoringBatteryOptimization((error,ignoringOptimization) => { 
+        console.log(error+' <<Android>>ignore>> '+ignoringOptimization) 
+       });
+     
+     const saveGPSData = (selectedDelivery) => {
+      console.log('Selected Delivery:' + selectedDelivery);
+     }
+
     const Stack = createStackNavigator();
-    
-    
-    // const [loading, setLoading] = useState(true);
-    // const [logged, setLogged] = useState(false);
-    // const [user, setUser] = useUserContext();
-    
-    // useEffect(() => {
-    //     LoadGlobalData();
-    // });
-    
-    // const LoadGlobalData = async () => {
-    //     setLoading(true);
-    //     if(!user || user.id === 0){
-    //         let asyncStorageUser: User = await GetGlobalUser();
-    //         if(!!asyncStorageUser && asyncStorageUser.id > 0){
-    //             setUser(asyncStorageUser);
-    //             setLogged(true);
-    //         }
-    //     }
-    //     setLoading(false);
-    //   };
-
-
     return (
         <NavigationContainer>
-        <Stack.Navigator initialRouteName="List">
-            <Stack.Screen name="List" component={DeliveriesList} />
-            <Stack.Screen name="Detail" component={DeliveryDetailScreen} />
+        <Stack.Navigator initialRouteName='List'>
+            <Stack.Screen name='List' component={DeliveriesList} />
+            <Stack.Screen name='Detail' component={DeliveryDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     );
