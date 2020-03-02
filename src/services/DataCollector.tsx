034660a547d85b0ee2c5e61364b5811fa89972bd
xesk84 <fj.aparisi@gmail.com>
2020-03-02 19:34:37 +0100

    Solved battery problem

--- src/services/DataCollector.tsx
+++ src/services/DataCollector.tsx
@@ -1,38 +1,49 @@
-import Geolocation, { GeolocationResponse } from '@react-native-community/geolocation';
+import Geolocation, {
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
 import DeviceInfo from 'react-native-device-info';
-import { Tracking } from '../entities';
-import { PowerState } from 'react-native-device-info/lib/typescript/internal/types';
+import {Tracking} from '../entities';
 import {postTrackings} from './api/TrackData';
 
 let tracking: Tracking = {driverId: 0, trackingDatas: []};
 
 export const collectData = (deliveryId: number, driverId: number) => {
-    getCurrentPosition(deliveryId, driverId);
+  getCurrentPosition(deliveryId, driverId);
 };
 
 const getCurrentPosition = (deliveryId: number, driverId: number): void => {
-    Geolocation.getCurrentPosition(geoLocation => {
-        buildTracking(deliveryId, driverId, geoLocation);
-     });
+  Geolocation.getCurrentPosition(geoLocation => {
+    getBatteryLevel(deliveryId, driverId, geoLocation);
+  });
 };
 
-const buildTracking = async (deliveryId: number, driverId: number, geoLocation: GeolocationResponse) => {
-
-    let batteryLevel = 0;
-    await DeviceInfo.getPowerState().then(battery => batteryLevel = battery.batteryLevel);
-
-    tracking.driverId = driverId;
-    tracking.trackingDatas.push({
-        deliveryId: deliveryId,
-        latitude: geoLocation.coords.latitude,
-        longitude: geoLocation.coords.longitude,
-        batteryLevel: batteryLevel * 100,
-        timestamp: geoLocation.timestamp
-    });
-    if(tracking.trackingDatas.length >= 10){
-        postTrackings(tracking);
-        tracking.trackingDatas = [];
-    }
+const getBatteryLevel = async (
+  deliveryId: number,
+  driverId: number,
+  geoLocation: GeolocationResponse,
+) => {
+  await DeviceInfo.getBatteryLevel().then(battery => {
+    buildTrackingData(deliveryId, driverId, geoLocation, battery);
+  });
 };
 
-
+const buildTrackingData = (
+  deliveryId: number,
+  driverId: number,
+  geoLocation: GeolocationResponse,
+  battery: number,
+) => {
+  tracking.driverId = driverId;
+  tracking.trackingDatas.push({
+    deliveryId: deliveryId,
+    latitude: geoLocation.coords.latitude,
+    longitude: geoLocation.coords.longitude,
+    batteryLevel: battery * 100,
+    timestamp: Date.now(),
+  });
+  
+  if (tracking.trackingDatas.length >= 10) {
+    postTrackings(tracking);
+    tracking.trackingDatas = [];
+  }
+};
